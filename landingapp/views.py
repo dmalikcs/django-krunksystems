72ccfb9fe7bd89f220cdae1356c3e1f288b448d4
dmalikcs <dmalikcs@gmail.com>
2013-08-17 02:36:12 +0530

    Added CRT_form object

--- landingapp/views.py
+++ landingapp/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext 
 from django.shortcuts import render_to_response
+from trainingapp.models import CorporateTrainingForm
 
 from contactus.models import A2F1_contactus,A2M1_contactus
 
@@ -9,6 +10,7 @@ def A1V1_landing(request):
     template_name="index.html"
     template_dic={}
     template_dic['contactus_form']=A2F1_contactus()
+    template_dic['CRT_Form']=CorporateTrainingForm()
     ci=RequestContext(request)
     return render_to_response(
             template_name,
