6a45cbefe67411100b0dfa9634c0c63bd83e8fb7
dmalikcs <dmalikcs@gmail.com>
2013-08-20 20:51:58 +0530

    new route added

--- static/js/routes.js
+++ static/js/routes.js
@@ -3,7 +3,13 @@ AboutusRouter = Backbone.Router.extend({
     'zero':'ZeroRoute',
     'dna':'DnaRoute',
     'idea':'IdeaRoute',
-    'letskrunk':'LetsKrunkRoute'
+    'letskrunk':'LetsKrunkRoute',
+    'crtform':'CRTRoute',
+    'about':'AboutRoute',
+    'service_training':'TrainingRoute',
+    'service_consulting':'ConsultingRoute',
+    'service_development':'DevelopementRoute',
+    'service_erp':'ErpRoute',
 
       },
     ZeroRoute:function(){
@@ -22,7 +28,39 @@ AboutusRouter = Backbone.Router.extend({
         console.log("called from  Let's Krunk Systems ");
         new LetskrunkView();
     },
+    CRTRoute:function(){
+        console.log("called from: Let's CoutRoute");
+        new CRTView();
+        console.log("called CRTView");
+    },
+    AboutRoute:function(){
+        console.log("called from: AboutRoute");
+        new AboutView();
+        console.log("called AboutView");
+    },
+    TrainingRoute:function(){
+        console.log("called from: TrainingRoute");
+        new TrainingView();
+        console.log("called TrainingView");
+    },
+    ConsultingRoute:function(){
+        console.log("called from: ConsultingRoute");
+        new ConsultingView();
+        console.log("called AboutView");
+    },
+    DevelopementRoute:function(){
+        console.log("called from: DevelopementRoute");
+        new DevelopementView();
+        console.log("called AboutView");
+    },
+    ErpRoute:function(){
+        console.log("called from: ErpRoute");
+        new ErpView();
+        console.log("called ErpView");
+    }
 });
 
-new AboutusRouter();
+aboutrouter=new AboutusRouter();
 Backbone.history.start();
+//aboutrouter.navigate('about', {trigger: true});
+//aboutrouter.navigate('service_training', {trigger: true});
