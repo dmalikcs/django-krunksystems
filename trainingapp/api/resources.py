6bfb81161321c3a193f72c04428b744d7c990f5c
dmalikcs <dmalikcs@gmail.com>
2013-08-20 20:53:41 +0530

    authntication added for training Apps

--- trainingapp/api/resources.py
+++ trainingapp/api/resources.py
@@ -1,23 +1,29 @@
 from tastypie.resources import ModelResource
 from tastypie.authentication import ApiKeyAuthentication
 from tastypie.authorization import Authorization
-from trainingapp.models import TrainingCourse,CorporateTraining,IndustrialTraining,AcademyTraining
+from trainingapp.models import TrainingCourse,CorporateTraining,IndustrialTraining,AcademyTraining,CorporateTrainingForm
+from tastypie.validation import Validation,FormValidation
 
 class TrainingCourseResource(ModelResource):
     class Meta:
         queryset=TrainingCourse.objects.all()
         resource_name='trainingcource'
         allowed_methods=['get','post']
-        #authentication=ApiKeyAuthentication()
+        authentication=ApiKeyAuthentication()
         #authorization=Authorization()
 
 class CorporateTrainingResource(ModelResource):
+    def hydrate(self,bundle):
+        print bundle.request.POST
+        return bundle
     class Meta:
         queryset=CorporateTraining.objects.all()
         resource_name='cor_training'
         allowed_methods=['get','post']
+        detail_allowed_methods=['post']
         authentication=ApiKeyAuthentication()
         authorization=Authorization()
+        validation=FormValidation(form_class=CorporateTrainingForm)
 
 class IndustrialTrainingResource(ModelResource):
     class Meta:
