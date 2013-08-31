18cf1077d7644775d7364659e5c4e2fdcb634e56
dmalikcs <dmalikcs@gmail.com>
2013-09-01 04:33:10 +0530

    events added for about

--- static/js/views.js
+++ static/js/views.js
@@ -1,130 +1,47 @@
 //ZeroView 
 AboutView=Backbone.View.extend({
-        el:$('#about_container'),
+        el:$('#aboutus'),
+        events:{
+        'click a#about_id':'AboutusView',
+        'click a#zero_id':'ZeroView',
+        'click a#idea_id':'IdeaView',
+        'click a#letskrunk_id':'LetsKrunkView',
+        'click a#dna_id':'DnaView',
+        },
+        AboutusView:function(event){
+            var zero =_.template($('#about_template').html(),{});
+            $('#about_container').html(zero);
+        },
+        ZeroView:function(event){
+            var zero =_.template($('#zero_template').html(),{});
+            $('#about_container').html(zero);
+        },
+        IdeaView:function(event){
+            var idea =_.template($('#idea_template').html(),{});
+            $('#about_container').html(idea);
+        },
+        LetsKrunkView:function(event){
+            var krunk =_.template($('#letskrunk_template').html(),{});
+            $('#about_container').html(krunk);
+        },
+        DnaView:function(event){
+            var dna =_.template($('#dna_template').html(),{});
+            $('#about_container').html(dna);
+        },
         initialize:function(){
             console.log("AboutView called");
             this.render();
+            this.AboutusView();
         },
         render:function(){
-            var zero =_.template($("#about_template").html(),{});
+            var zero =_.template($("#aboutus_template").html(),{});
             this.$el.html(zero);
         },
 });
-
-ZeroView=Backbone.View.extend({
-        el:$('#about_container'),
-        initialize:function(){
-            console.log("zeroView called");
-            this.render();
-        },
-        render:function(){
-            var zero =_.template($("#zero_template").html(),{});
-            this.$el.html(zero);
-        },
-});
-
-//DnaView
-DnaView=Backbone.View.extend({
-        el:$('#about_container'),
-        initialize:function(){
-            console.log("DnaView called");
-            this.render();
-        },
-        render:function(){
-            var zero =_.template($("#dna_template").html(),{});
-            this.$el.html(zero);
-        },
-
-});
-
-//IdeaView
-IdeaView=Backbone.View.extend({
-        el:$('#about_container'),
-        initialize:function(){
-            console.log("IdeaView called");
-            this.render();
-        },
-        render:function(){
-            var zero =_.template($("#idea_template").html(),{});
-            this.$el.html(zero);
-
-        },
-
-});
-
-//LetskrunkView
-LetskrunkView=Backbone.View.extend({
-        el:$('#about_container'),
-        initialize:function(){
-            console.log("LetskrunksystemsView called");
-            this.render();
-        },
-        render:function(){
-            var zero =_.template($("#letskrunk_template").html(),{});
-            this.$el.html(zero);
-        },
-        });
-// Testing 
-var CRTView=Backbone.View.extend({
-    //el:
-    el:$('#service_container'),
-    initialize:function(){
-            console.log("CRTView called");
-            this.render();
-        },
-    render:function(){
-           var zero =_.template($("#CRTtemplate").html(),{});
-          this.$el.html(zero);
-    },
-    events:{
-    'click button.btn-primary':'CRTAdd',
-    },
-    CRTAdd:function(event){
-            console.log("Workinf");
-            company_name=$('#id_company_name').val();
-            employee_name=$('#id_employee_name').val();
-            personal_email=$('#id_personal_email').val();
-            offical_email=$('#id_offical_email').val();
-            mobile=$('#id_mobile').val();
-            phone=$('#id_phone').val();
-            message=$('#id_message').val();
-    console.log(message);    
-    var CRTmodel=new CRTModel({
-               company_name:company_name,
-                employee_name:employee_name,
-                personal_email:personal_email,
-                offical_email:offical_email,
-                mobile:mobile,
-                phone:phone,
-                message:message,
-          })
-        console.log("console");
-        CRTmodel.save();
-        return false;
-        },
-});
-
-/*courseView=Backbone.View.extend({
-        el:$('#coursetraining'),
-        initialize:function(){
-            var cc=new CourseCollection()
-            cc.fetch({
-                    success:function(collection,response){
-                    self.render();
-                    },
-        });
-        },
-        render:function(){
-           console.log("render function called from");
-           var zero =_.template("Hello",{});
-           this.$el.html(zero);
-        },
-});*/
 
 TrainingView=Backbone.View.extend({
         el:$('#service_container'),
         initialize:function(){
-            console.log("TraiingView called");
             this.render();
         },
         render:function(){
@@ -135,9 +52,6 @@ TrainingView=Backbone.View.extend({
         'click a.test':'CourseDisplay',
         'click a.Deepak':'SingleCourseDisplay',
         'click button.btn-primary':'CRTAdd',
-        },
-        SingleCourseDisplay:function(event){
-            
         },
         CourseDisplay:function(events){
             var cc=new CourseCollection();
@@ -157,7 +71,6 @@ TrainingView=Backbone.View.extend({
             mobile=$('#id_mobile').val();
             phone=$('#id_phone').val();
             message=$('#id_message').val();
-    console.log(message);    
     var CRTmodel=new CRTModel({
                company_name:company_name,
                 employee_name:employee_name,
@@ -195,7 +108,7 @@ DevelopementView=Backbone.View.extend({
         },
 });
 
-ErpView=Backbone.View.extend({
+var ErpView=Backbone.View.extend({
         el:$('#service_container'),
         events:{
             'click a.erp_modules':'ERPModulesDisplay',
@@ -216,9 +129,7 @@ ErpView=Backbone.View.extend({
                         var data=_.template($('#modules_template').html(),{modules:response.objects});
                         $('#modules').html(data);
                         },
-
             });
-            
         },
 });
 
@@ -226,38 +137,12 @@ var CourseView=Backbone.View.extend({
     el:$('#temp'),
     initialize:function(){
         console.log("ListCoursePage called");
-        //_.bindAll(this,'render','ListCoursePage');
         this.render()
-
     },
     render:function(response){
         console.log("render function called");
         var temp=_.template($('#crs'),{});
         this.$el.html(temp);
     },
- /*   ListCoursePage:function(querystring){
-        var self = this;
-        var cc=new CourseCollection()
-        cc.fetch({
-            data:querystring,
-            success:function(collection,response){
-            self.render(response);
-        },
-    });
-    },
-*/
 });
 
-
-/*
-    console.log("CourseDisplay function called");
-    var self = this;
-    var cc=new CourseCollection()
-    cc.fetch({
-        success:function(collection,response){
-        var zero =_.template("Hello",{});
-        self.$el.html(zero);
-        self.render_new(response);
-    },
-    });
-*/
