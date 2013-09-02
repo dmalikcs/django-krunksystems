d592f917c0329ba391e3983a319943cdcb2c7344
dmalikcs <dmalikcs@gmail.com>
2013-09-03 00:28:44 +0530

    Modules added

--- trainingapp/admin.py
+++ trainingapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin 
-from .models import TrainingCourse,CorporateTraining,AcademyTraining,IndustrialTraining
+from .models import TrainingCourse,CorporateTraining,AcademyTraining,IndustrialTraining,TrainingSuccess,CourseOutline,MentorDetail
 
 admin.site.register(TrainingCourse)
 admin.site.register(CorporateTraining)
 admin.site.register(AcademyTraining)
 admin.site.register(IndustrialTraining)
+admin.site.register(TrainingSuccess)
+admin.site.register(CourseOutline)
+admin.site.register(MentorDetail)
