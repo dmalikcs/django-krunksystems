94073c47b8a927e875cfa09729b1f78a126a3197
dmalikcs <dmalikcs@gmail.com>
2013-09-03 02:18:51 +0530

    admin added for whythistraining

--- trainingapp/admin.py
+++ trainingapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from .models import TrainingCourse,CorporateTraining,AcademyTraining,IndustrialTraining,TrainingSuccess,CourseOutline,MentorDetail
+from .models import TrainingCourse,CorporateTraining,AcademyTraining,IndustrialTraining,TrainingSuccess,CourseOutline,MentorDetail,WhyThisTraining
 
 admin.site.register(TrainingCourse)
 admin.site.register(CorporateTraining)
@@ -8,3 +8,4 @@ admin.site.register(IndustrialTraining)
 admin.site.register(TrainingSuccess)
 admin.site.register(CourseOutline)
 admin.site.register(MentorDetail)
+admin.site.register(WhyThisTraining)
