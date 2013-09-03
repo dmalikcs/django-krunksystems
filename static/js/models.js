2aac6a3c28d6571dd128715ecae5b9c79cb138d6
dmalikcs <dmalikcs@gmail.com>
2013-09-03 20:42:33 +0530

    models defined for ITModel/ATModel

--- static/js/models.js
+++ static/js/models.js
@@ -18,32 +18,150 @@ sync: function (method, model, options){
                 }
                 ),
                 beforeSend: function(){
-                    $('#id_err_company_name').html('');
+                    $('#crform #id_err_company_name').html('');
+                    $('#crform #id_err_employee_name').html('');
+                    $('#crform #id_err_personal_email').html('');
+                    $('#crform #id_err_offical_email').html('');
+                    $('#crform #id_err_mobile').html('');
+                    $('#crform #id_err_phone').html('');
+                    $('#crform #id_err_message').html('');
                 },
                 success:function(data){
-                    //$('#crform').html("<button class = \"btn btn-danger\" data-dismiss=\"modal\" aria-hidden=\"true\"> Close </button>");
                     var a=_.template($('#crform').html(),{});
                     $('#crform').html(a);
-                    var alert = _.template($('#alert-template').html(),{
+                    var alert = _.template($('#alert-template').html(),{ //loading from templates.html
                     type:'success',
-                    error:'Won',
-                    error_message:'Yeh,We wone it',
+                    bold_message:'Thanks ',
+                    general_message:'Yeh,We wone it',
                     });
                     console.log(alert);
-                    $('#saved').html(alert);
+                    $('#crform #success-saved').html(alert);
                 },
                 error:function(jqXHR, textStatus, errorThrown){
                     var errors = JSON.parse(jqXHR.responseText) 
                     for (field in errors.cor_training) {
                         var error = errors['cor_training'][field];
-                        $('#id_err_' + field).html(" " + error + " ");
+                        $('#crform #id_err_' + field).html(" " + error + " ");
                     }   
-                },
-                
+                },//error function closed
         });
-    }
-},
+    }//if loop closed
+},//sync function closed
 });//CRTModel Closed
+
+
+/* Industrial Training Model*/
+var ITModel=Backbone.Model.extend({     
+sync: function (method, model, options){
+    if (method === 'create'){
+         return $.ajax({
+                datatype:'json',
+                type:'POST',
+                contentType: 'application/json',
+                url:'/api/v1/id_training/?username=dmalik5\&api_key=4791c10acb8d425fbab86dc05adc49087d3050c2',
+                global: true,
+                data: JSON.stringify({
+                        student_name:(this.get('student_name')|| ''),
+                        email:(this.get('email')|| ''),
+                        mobile:(this.get('mobile')|| ''),
+                        collage:(this.get('collage')|| ''),
+                        degree:(this.get('degree')|| ''),
+                        intership_period:(this.get('intership_period')|| ''),
+                        platform:(this.get('platform')|| ''),
+                        message:(this.get('message')|| ''),
+                }
+                ),
+                beforeSend: function(){
+                        $('#itform #id_err_student_name').html('');
+                        $('#itform #id_err_email').html('');
+                        $('#itform #id_err_mobile').html('');
+                        $('#itform #id_err_collage').html('');
+                        $('#itform #id_err_degree').html('');
+                        $('#itform #id_err_intership_period').html('');
+                        $('#itform #id_err_platform').html('');
+                        $('#itform #id_err_message').html('');
+                },
+                success:function(data){
+                    var a=_.template($('#itform').html(),{});
+                    $('#itform').html(a);
+                    var alert = _.template($('#alert-template').html(),{ //loading from templates.html
+                    type:'success',
+                    bold_message:'Thanks ',
+                    general_message:' Yeh,We wone it',
+                    });
+                    console.log(alert);
+                    $('#itform #success-saved').html(alert);
+                },
+                error:function(jqXHR, textStatus, errorThrown){
+                    var errors = JSON.parse(jqXHR.responseText) 
+                    console.log(errors);
+                    for (field in errors.id_training) {
+                        var error = errors['id_training'][field];
+                        $('#itform #id_err_' + field).html(" " + error + " ");
+                        //$('#itform #id_err_' + field).html(" " + field + " ");
+                    }   
+                },//error function closed
+        });
+    }//if loop closed
+},//sync function closed
+});//ITModel Closed
+
+var ATModel=Backbone.Model.extend({     
+sync: function (method, model, options){
+    if (method === 'create'){
+         return $.ajax({
+                datatype:'json',
+                type:'POST',
+                contentType: 'application/json',
+                url:'/api/v1/ac_training/?username=dmalik5\&api_key=4791c10acb8d425fbab86dc05adc49087d3050c2',
+                global: true,
+                data: JSON.stringify({
+                    student_name:(this.get('student_name')|| ''),
+                    email:(this.get('email')|| ''),
+                    degree:(this.get('degree')|| ''),
+                    branch:(this.get('branch')|| ''),
+                    mobile:(this.get('mobile')|| ''),
+                    collage_name:(this.get('collage_name')|| ''),
+                    collage_website:(this.get('collage_website')|| ''),
+                    message:(this.get('message')|| ''),
+                }),
+                beforeSend: function(){
+                
+                    $('#atform #id_err_student_name').html('');
+                    $('#atform #id_err_email').html('');
+                    $('#atform #id_err_degree').html('');
+                    $('#atform #id_err_branch').html('');
+                    $('#atform #id_err_mobile').html('');
+                    $('#atform #id_err_collage_name').html('');
+                    $('#atform #id_err_collage_website').html('');
+                    $('#atform #id_err_message').html('');
+                },
+                success:function(data){
+                    var a=_.template($('#atform').html(),{});
+                    $('#atform').html(a);
+                    var alert = _.template($('#alert-template').html(),{ //loading from templates.html
+                    type:'success',
+                    bold_message:'Thanks ',
+                    general_message:' Yeh,We wone it',
+                    });
+                    console.log(alert);
+                    $('#atform #success-saved').html(alert);
+                },
+                error:function(jqXHR, textStatus, errorThrown){
+                    var errors = JSON.parse(jqXHR.responseText) 
+                    console.log(errors);
+                    for (field in errors.ac_training) {
+                        var error = errors['ac_training'][field];
+                        console.log('#id_err_'+field);
+                        $('#atform #id_err_' + field).html(" " + error + " ");
+                    }   
+                },//error function closed
+        });
+    }//if loop closed
+},//sync function closed
+});//ATModel Closed
+
+
 
 var CourseModel=Backbone.Model.extend({
     url:'/api/v1/courses/?format=json'
