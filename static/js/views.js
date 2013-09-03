7f23cfd26b7ef8d32a588c2b126e3d77d2db7f8b
dmalikcs <dmalikcs@gmail.com>
2013-09-03 20:43:05 +0530

    views defined for ITModel/ATModel

--- static/js/views.js
+++ static/js/views.js
@@ -49,9 +49,10 @@ TrainingView=Backbone.View.extend({
             this.$el.html(zero);
         },
         events:{
-        'click a.test':'CourseDisplay',
-        'click a.Deepak':'SingleCourseDisplay',
-        'click button.btn-primary':'CRTAdd',
+        'click a.coursedisplay':'CourseDisplay',
+        'click #crform_submit':'CRTAdd',
+        'click #itform_submit':'ITAdd',
+        'click #atform_submit':'ATAdd',
         },
         CourseDisplay:function(events){
             var cc=new CourseCollection();
@@ -63,14 +64,13 @@ TrainingView=Backbone.View.extend({
                     });
         } ,
     CRTAdd:function(event){
-            console.log("Workinf");
-            company_name=$('#id_company_name').val();
-            employee_name=$('#id_employee_name').val();
-            personal_email=$('#id_personal_email').val();
-            offical_email=$('#id_offical_email').val();
-            mobile=$('#id_mobile').val();
-            phone=$('#id_phone').val();
-            message=$('#id_message').val();
+            company_name=$('#crform #id_company_name').val();
+            employee_name=$('#crform #id_employee_name').val();
+            personal_email=$('#crform #id_personal_email').val();
+            offical_email=$('#crform #id_offical_email').val();
+            mobile=$('#crform #id_mobile').val();
+            phone=$('#crform #id_phone').val();
+            message=$('#crform #id_message').val();
     var CRTmodel=new CRTModel({
                company_name:company_name,
                 employee_name:employee_name,
@@ -83,6 +83,54 @@ TrainingView=Backbone.View.extend({
         CRTmodel.save();
         return false;
         },
+    ITAdd: function(){ //ITadd training 
+        student_name=$('#itform #id_student_name').val();
+        email=$('#itform #id_email').val();
+        mobile=$('#itform #id_mobile').val();
+        collage=$('#itform #id_collage').val();
+        degree=$('#itform #id_degree').val();
+        intership_period=$('#itform #id_intership_period').val();
+        platform=$('#itform #id_platform').val();
+        message=$('#itform #id_message').val();
+    var ITmodel=new ITModel({
+        student_name:student_name,
+        email:email,
+        mobile:mobile,
+        collage:collage,
+        degree:degree,
+        intership_period:intership_period,
+        platform:platform,
+        message:message,
+      });
+        ITmodel.save();
+        return false;
+            
+    },
+    ATAdd:function(){ //ATAdd Training
+        console.log('AT add called');
+        /*AcademyTraining */
+        student_name=$('#atform #id_student_name').val();
+        email=$('#atform #id_email').val();
+        degree=$('#atform #id_degree').val();
+        branch=$('#atform #id_branch').val();
+        mobile=$('#atform #id_mobile').val();
+        collage_name=$('#atform #id_collage_name').val();
+        collage_website=$('#atform #id_collage_website').val();
+        message=$('#atform #id_message').val();
+        console.log(message);
+    var ATmodel=new ATModel({
+        student_name:student_name,
+        email:email,
+        degree:degree,
+        branch:branch,
+        mobile:mobile,
+        collage_name:collage_name,
+        collage_website:collage_website,
+        message:message
+        });
+        ATmodel.save();
+        return false;
+    },
 });
 
 ConsultingView=Backbone.View.extend({
@@ -90,6 +138,24 @@ ConsultingView=Backbone.View.extend({
         initialize:function(){
             console.log("ConsultView called");
             this.render();
+        },
+        events:{
+        'click #pcform_submit':'PCAdd',
+        'click #dcform_submit':'DCAdd',
+        'click #ocform_submit':'OCAdd',
+        'click #mcform_submit':'MCAdd',
+        },
+        PCAdd:function(event){
+            console.log('PCAdd consultancy');
+        },
+        DCAdd:function(event){
+            console.log('DCAdd consultancy');
+        },
+        OCAdd:function(event){
+            console.log('OCAdd consultancy');
+        },
+        MCAdd:function(event){
+            console.log('MCAdd consultancy');
         },
         render:function(){
             var zero =_.template($("#service_consulting_template").html(),{});
@@ -102,6 +168,16 @@ DevelopementView=Backbone.View.extend({
             console.log("DevelopmentView called");
             this.render();
         },
+        events:{
+        'click #adform_submit':'ADAdd',
+        'click #wdform_submit':'WDAdd',
+        },
+        ADAdd:function(event){
+            console.log('ADAdd Click')
+        },
+        WDAdd:function(event){
+            console.log('WDAdd Click')
+        },
         render:function(){
             var zero =_.template($("#service_development_template").html(),{});
             this.$el.html(zero);
@@ -111,7 +187,15 @@ DevelopementView=Backbone.View.extend({
 var ErpView=Backbone.View.extend({
         el:$('#service_container'),
         events:{
+            'click #erpinquiryform_submit':'ErpInquiryAdd',
+            'click #demoform_submit':'DemoAdd',
             'click a.erp_modules':'ERPModulesDisplay',
+        },
+        ErpInquiryAdd:function(event){
+            console.log('ERP Inquiry submited')
+        },
+        DemoAdd:function(event){
+            console.log('Demo Called')
         },
         initialize:function(){
             console.log("ERPView called");
@@ -145,4 +229,3 @@ var CourseView=Backbone.View.extend({
         this.$el.html(temp);
     },
 });
-
